128b3721fece31435c12a6e9b217850fd5f4d2f7
Irina <131948761+IrinaMungalov@users.noreply.github.com>
2024-02-15 22:28:44 +0200

    Add files via upload

---
+++ booking/src/index.php
@@ -0,0 +1,8 @@
+<h1>Welcome to our HOTEL Booking</h1>
+<?php 
+  $days_till_openning = 30;  // snake_case_name
+  $bonusPlaces = 10;         // camelCase
+?>
+<p>Days till opening <?php print($days_till_openning) ?></p>
+<p>Bonus places left <?php print($bonusPlaces) ?></p>
+<p>Don't hesitate to early book us!</p>
