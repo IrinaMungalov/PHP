514e3047579f9597ad2c93d38fcb8672be9ea10c
Irina <131948761+IrinaMungalov@users.noreply.github.com>
2024-03-18 13:35:26 +0200

    Add files via upload

--- booking/src/examples/offer/index.php
+++ booking/src/examples/offer/index.php
@@ -5,22 +5,14 @@
     // include (всегда включает файл, даже если он уже был включен ранее)
     // include_once (проверяет, был ли файл включен ранее, и включает его только в случае отсутствия)
     // require (приводит к фатальной ошибке, если файл не найден, прекратит выполнение скрипта)
-    // require_once (проверяет, был ли файл уже включен, и включает его только в случае необходимости)  
-    
+    // require_once (проверяет, был ли файл уже включен, и включает его только в случае необходимости)      
 
     //  HW3: add 2 links on top of catalog ^ - sort ascending  v - sort descending
 
-    if (isset ($_GET['sort'])) {
-        $sort = $_GET['sort'];
-    } else {
-        $sort = ''; 
-    }
+    $sort = isset ($_GET['sort']) ? $_GET['sort'] : '';
 
-    if ($sort == 'asc') {
-        usort($products, fn($p1, $p2) => $p1['price']['amount'] - $p2['price']['amount']);
-    } else if ($sort == 'desc') {
-        usort($products, fn($p1, $p2) => $p2['price']['amount'] - $p1['price']['amount']);
-    }
+    usort($products, $sort == 'asc' ? fn($p1, $p2) => $p1['price']['amount'] - $p2['price']['amount']
+                                    : fn($p1, $p2) => $p2['price']['amount'] - $p1['price']['amount']);
 
 ?>
 
@@ -36,58 +28,39 @@
 </style>
 
 <div class="container">
-    <h2>Sorting: </h2>
+    <h2>Sort by price: </h2>
     <a href="?sort=asc">▲</a> 
     <a href="?sort=desc">▼</a>
 </div>
 
 <ol>
-    <?
-    
-    if ($page == 1) {
+    <?    
+        $product_count_per_page = 3;
+        $start_i = ($page - 1) * $product_count_per_page;
+        $end_i = ($page * $product_count_per_page < count($products)) ? $page * $product_count_per_page : count($products);
 
-        for ($i = 0; $i < 3 ; $i++) { ?>
-            <li>
-                <h2>
-                    <?= $products[$i]['name'] ?>
-                    <? if($products[$i]['new']) { ?> <img src="<?=NEW_STICKER ?>" width="50"> <? } ?>
-                </h2>
-                <h3>
-                    <?= $products[$i]['category'] ?>
-                </h3>
-                <img src="<?= $products[$i]['image'] ?>" width="200">
-                <div>
-                    <?= $products[$i]['price']['amount'] ?>
-                    <?= $products[$i]['price']['currency'] ?>
-                </div>
-                <hr>
-            </li>
-        <?
-        }
-
-    } else {
-        
-        for ($i = 3; $i < 6; $i++) { ?>
-            <li>
-                <h2>
-                    <?= $products[$i]['name'] ?>
-                </h2>
-                <h3>
-                    <?= $products[$i]['category'] ?>
-                </h3>
-                <img src="<?= $products[$i]['image'] ?>" width="200">
-                <div>
-                    <?= $products[$i]['price']['amount'] ?>
-                    <?= $products[$i]['price']['currency'] ?>
-                </div>
-                <hr>
-            </li>
-        <?
-        }
-
-    }
+        for ($i = $start_i; $i < $end_i; $i++) {
+             $product = $products[$i]
     ?>
-    
+
+    <li>
+        <h2>
+            <?= $product['name'] ?>
+            <? if ($product['new']) { ?> <img src="<?= NEW_STICKER ?>" width="50"> <? } ?>
+        </h2>
+        <h3>
+            <?= $product['category'] ?>
+        </h3>
+        <img src="<?= $product['image'] ?>" width="200">
+        <div>
+            <?= $product['price']['amount'] ?>
+            <?= $product['price']['currency'] ?>
+        </div>
+        <hr>
+    </li>
+
+    <? } ?>
+        
 </ol>
 
 <div class="container">
