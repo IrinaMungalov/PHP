5b88a08b98dc3d49f9b0d3c42a2052cd0b450de1
Irina <131948761+IrinaMungalov@users.noreply.github.com>
2024-08-22 10:30:24 +0300

    Add files via upload

---
+++ Booking_L4/Booking project v2.0/website/index.php
@@ -0,0 +1,40 @@
+<?
+    // phpinfo();
+    // die();
+    // for class autoloading do not remove !!!
+    require_once './vendor/autoload.php';
+
+    // TWIG INIT + CONFIG
+    $loader = new \Twig\Loader\FilesystemLoader('./templates');
+
+    $twig = new \Twig\Environment($loader, [
+        // 'cache' => '/path/to/compilation_cache',
+    ]);
+
+    $title = 'Booking Agency';
+
+
+    // connect to db / fetch data
+    // PHP 8.2+ ---> only mysql pdo / no "mysqli" !!!
+    $pdo = new PDO("mysql:host=booking_mariadb;dbname=booking;port=3306", "booking", "booking");
+
+    $stmt = $pdo->query('SELECT * FROM tours');
+
+    $tours = [];
+
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $tours[] = $row;
+    }
+
+    // var_dump($tours);
+    // die();
+
+    // render a simple page
+    $template = $twig->load('home.html.twig');
+
+    
+
+    print($template->render([
+        'title' => $title,
+        'tours'=> $tours
+    ]));
