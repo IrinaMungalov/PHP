7356ec051bfd40212ef2cbb9259d75d3a9d651a5
Irina <131948761+IrinaMungalov@users.noreply.github.com>
2024-10-10 14:37:04 +0300

    Add files via upload

--- Booking_L4/Booking project v2.0/sql/ddl.sql
+++ Booking_L4/Booking project v2.0/sql/ddl.sql
@@ -11,6 +11,47 @@ CREATE TABLE money (
     currency VARCHAR(4)
 );
 
+CREATE TABLE clients (
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
+    fullName VARCHAR(100),
+    contactEmail VARCHAR(100),
+    contactPhone VARCHAR(20)
+);
+
+CREATE TABLE orders (
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
+    state ENUM('draft', 'booked', 'completed', 'canceled'),
+    cost_id int,
+    client_id int,
+    FOREIGN KEY (cost_id) REFERENCES money (id),
+    FOREIGN KEY (client_id) REFERENCES clients (id)
+);
+
+CREATE TABLE order_items (
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
+    order_id int,
+    tour_id int,
+    cost_id int,
+    touristCount INT CHECK ( touristCount BETWEEN 1 AND 100 ), -- HW1:constrain this to 1..100
+    FOREIGN KEY (cost_id) REFERENCES money (id),
+    FOREIGN KEY (order_id) REFERENCES orders (id),
+    FOREIGN KEY (tour_id) REFERENCES tours (id)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 CREATE TABLE reviews (
     id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
     author_name VARCHAR(100),
